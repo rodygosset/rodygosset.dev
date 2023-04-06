919e6ecafd028f10d931a5f200c19f245752c244
Rody Gosset <74539903+rodygosset@users.noreply.github.com>
2023-04-06 23:09:16 +0200

    Added metadata

--- pages/index.tsx
+++ pages/index.tsx
@@ -71,20 +71,20 @@ const Home: NextPage<Props> = (
 	return (
 		<>
 			<Head>
-				<title>{ metaTitle }</title>
+				<title>Rody Gosset&apos;s portfolio</title>
 				<meta name="description" content={metaDescription} />
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<link rel="shortcut icon" href="favicon/favicon.svg" type="img/svg" />
 				<link rel="apple-touch-icon" sizes="180x180" href="/favicon/favicon-180x180.png" />
 				<link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png"/>
 				<link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png"/>
-				<meta name="og:title" content={metaTitle} />
-				<meta name="twitter:title" content={metaTitle} />
-				<meta name="og:type" content="website" />
-				<meta name="og:description" content={metaDescription} />
-				<meta name="twitter:description" content={metaDescription} />
-				<meta name="og:url" content="https://www.rodygosset.dev/" />
-				<meta name="og:image" content="https://www.rodygosset.dev/images/my-memoji-rectangle.png" />
+				<meta property="og:title" content={metaTitle} />
+				<meta property="twitter:title" content={metaTitle} />
+				<meta property="og:type" content="website" />
+				<meta property="og:description" content={metaDescription} />
+				<meta property="twitter:description" content={metaDescription} />
+				<meta property="og:url" content="https://www.rodygosset.dev/" />
+				<meta property="og:image" content="https://www.rodygosset.dev/images/my-memoji-rectangle.png" />
 			</Head>
 			<Header content={getLocaleNavContent()} />
 			<main id={styles.main}>
