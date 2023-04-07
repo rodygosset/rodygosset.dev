b29fa051d0a0ce9e699d2c309eae9eb99700977a
Rody Gosset <74539903+rodygosset@users.noreply.github.com>
2023-04-07 12:26:21 +0200

    Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,16 @@ import '@styles/globals.scss'
 // font awesome setup
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
+import { Analytics } from '@vercel/analytics/react';
 config.autoAddCss = false
 
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<Component {...pageProps} />
+			<Analytics/>
+		</>
+	)
 }
